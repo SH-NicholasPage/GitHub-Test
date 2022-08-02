340a9e8f7fa1231189121c2d5d8b385577e483f9
SH-NicholasPage <npage@southhills.edu>
2022-08-01 17:52:06 -0700

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        static String name = "YOUR NAME HERE";//This is the only line you should edit in this lab
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
